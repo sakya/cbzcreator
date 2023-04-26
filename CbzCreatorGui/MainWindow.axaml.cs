05a3262614ada56393fc44bf4fded332764dc0bf
Paolo Iommarini <paolo.iommarini@xenialab.it>
2023-04-26 15:45:44 +0200

    Added version info

--- CbzCreatorGui/MainWindow.axaml.cs
+++ CbzCreatorGui/MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
@@ -15,6 +16,7 @@ namespace CbzCreatorGui
         {
             InitializeComponent();
 
+            Title = $"{Title} - v{Assembly.GetExecutingAssembly().GetName().Version!.ToString()}";
             var statuses = new List<NameValue>()
             {
                 new("Unknown", Info.Statuses.Unknown),
---
+++ publish.bat
@@ -0,0 +1,8 @@
+rd /s /q .\dist\win-x64
+rd /s /q .\CbzCreator.Lib\bin
+rd /s /q .\CbzCreator.Lib\obj
+rd /s /q .\CbzCreatorGui\bin
+rd /s /q .\CbzCreatorGui\obj
+
+dotnet clean CbzCreator.sln -c Release
+dotnet publish CbzCreator.sln -c Release --runtime win-x64 -p:PublishReadyToRun=true --self-contained --output .\dist\win-x64
